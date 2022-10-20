b7d3f58b725f2bb94cc05aaa4a34752f625b5061
Jean Forero <tausertholland@gmail.com>
2022-10-20 10:08:15 -0500

    JarcoApp

---
+++ components/Layout.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+
+export default function Layout({ title, children }) {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + ' -Jarco' : 'Jarco'}</title>
+        <meta name="description" content="Ecommerce Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 justify-between">
+            <Link href="/">
+              <a className="text-lg font-bold">Jarco</a>
+            </Link>
+            <div>
+              <Link href="/cart">Cart</Link>
+              <Link href="/login">Login</Link>
+            </div>
+          </nav>
+        </header>
+        <main>{children}</main>
+        <footer>footer</footer>
+      </div>
+    </>
+  );
+}
